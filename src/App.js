575d6e248d873ec095c1c17c97e287c558e3fb77
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-03-31 12:47:54 +0600

    Created the footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Articles from './Components/Articles/Articles';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Articles></Articles>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>1. What are the differences between props and state? <br /> = </p>
+            <p>2. How does useState work?<br /> =</p>
+            <p>3. What can useEffect be used for except loading data?<br /> =</p>
+            <p>4. How does react work?<br /> =</p>
+        </div>
+    );
+};
+
+export default Footer;
